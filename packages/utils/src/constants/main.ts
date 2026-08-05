48dc242461eab936510098518b7b5c8bfc79ccc4
idanielvolkov <tools.idanielvolkov@gmail.com>
2026-08-06 00:10:50 +0300

    Use self-hosted application domains

--- packages/utils/src/constants/main.ts
+++ packages/utils/src/constants/main.ts
@@ -1,6 +1,10 @@
-export const SHORT_DOMAIN = "dub.sh";
+export const SHORT_DOMAIN =
+  process.env.NEXT_PUBLIC_APP_SHORT_DOMAIN || "dub.sh";
+
+const APP_HOSTNAME = process.env.NEXT_PUBLIC_APP_DOMAIN || "app.dub.co";
 
 export const APP_HOSTNAMES = new Set([
+  APP_HOSTNAME,
   "app.dub.co",
   "preview.dub.co",
   "localhost:8888",
@@ -9,14 +13,14 @@ export const APP_HOSTNAMES = new Set([
 
 export const APP_DOMAIN =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? "https://app.dub.co"
+    ? `https://${APP_HOSTNAME}`
     : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
       ? "https://preview.dub.co"
       : "http://localhost:8888";
 
 export const APP_DOMAIN_WITH_NGROK =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? "https://app.dub.co"
+    ? `https://${APP_HOSTNAME}`
     : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
       ? process.env.VERCEL_URL
         ? `https://${process.env.VERCEL_URL}`
@@ -33,18 +37,20 @@ export const API_HOSTNAMES = new Set([
 
 export const API_DOMAIN =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? "https://api.dub.co"
+    ? `https://api.${SHORT_DOMAIN}`
     : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
       ? "https://api-staging.dub.co"
       : "http://api.localhost:8888";
 
 export const ADMIN_HOSTNAMES = new Set([
+  `admin.${SHORT_DOMAIN}`,
   "admin.dub.co",
   "admin.localhost:8888",
   "admin.localhost",
 ]);
 
 export const PARTNERS_HOSTNAMES = new Set([
+  `partners.${SHORT_DOMAIN}`,
   "partners.dub.co",
   "partners-staging.dub.co",
   "partners.localhost:8888",
@@ -53,14 +59,14 @@ export const PARTNERS_HOSTNAMES = new Set([
 
 export const PARTNERS_DOMAIN =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? "https://partners.dub.co"
+    ? `https://partners.${SHORT_DOMAIN}`
     : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
       ? "https://partners-staging.dub.co"
       : "http://partners.localhost:8888";
 
 export const PARTNERS_DOMAIN_WITH_NGROK =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? "https://partners.dub.co"
+    ? `https://partners.${SHORT_DOMAIN}`
     : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
       ? "https://partners-staging.dub.co"
       : process.env.NEXT_PUBLIC_NGROK_URL || "http://partners.localhost:8888";
@@ -74,7 +80,7 @@ export const DUB_THUMBNAIL = "https://assets.dub.co/thumbnail.jpg";
 export const DUB_WORKSPACE_ID = "cl7pj5kq4006835rbjlt2ofka";
 export const ACME_WORKSPACE_ID = "clrei1gld0002vs9mzn93p8ik";
 export const ACME_PROGRAM_ID = "prog_CYCu7IMAapjkRpTnr8F1azjN";
-export const LEGAL_WORKSPACE_ID = "clrflia0j0000vs7sqfhz9c7q";
+export const LEGAL_WORKSPACE_ID = "clrflia0j0000vsnzi01pbf47";
 export const LEGAL_USER_ID = "clqei1lgc0000vsnzi01pbf47";
 
 export const R2_URL = process.env.STORAGE_BASE_URL || "https://dubassets.com";
